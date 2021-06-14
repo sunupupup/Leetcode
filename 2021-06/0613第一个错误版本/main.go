327d91c9610609c396dea0d033d486407bf0ef7f
sjw <2293261394@qq.com>
2021-06-14 16:26:47 +0800

    二分

---
+++ 2021-06/0613第一个错误版本/main.go
@@ -0,0 +1,22 @@
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	left := 1
+	right := n
+	for left < right {
+		mid := (right-left)/2 + left
+		if isBadVersion(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
