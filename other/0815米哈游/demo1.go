6bbe4f59476d8ffbf32c06af18bfc295720ced21
sjw <2293261394@qq.com>
2021-08-15 21:06:03 +0800

    demo1

---
+++ other/0815米哈游/demo1.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	n := 0
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ {
+
+		s := ""
+		fmt.Scan(&s)
+		bs := []byte(s)
+		count := 0
+		for _, b := range bs {
+			if b == 'a' {
+				count++
+			} else {
+				count--
+			}
+			if count < 0 {
+				fmt.Println("NO")
+                break
+			}
+		}
+		if count == 0 {
+			fmt.Println("YES")
+        }else if count>  0 {
+            fmt.Println("NO")
+        }
+
+	}
+
+}
