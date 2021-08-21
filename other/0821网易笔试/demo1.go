effb760e7d3e6bb23ea06e8c83e6b38878b784e9
sjw <2293261394@qq.com>
2021-08-21 15:10:30 +0800

    demo1

---
+++ other/0821网易笔试/demo1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	temp := []int{}
+	num := 0
+	for {
+		_, err := fmt.Scan(&num)
+		if err != nil {
+			break
+		}
+		temp = append(temp, num)
+	}
+
+	n := len(temp) - 1
+	nums := temp[:n]
+	target := temp[n]
+
+	ret := 0
+	for i := 0; i < n; i++ {
+		a := nums[i]
+		for j := i + 1; j < n; j++ {
+			b := nums[j]
+			if a+b <= target {
+				ret++
+			}
+		}
+	}
+
+	fmt.Println(ret)
+}
